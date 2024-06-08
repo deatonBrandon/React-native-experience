744a1b051f901e1386cb3339486fa92f2482e71d
brandon-deaton <dream.cloud936@gmail.com>
2024-06-08 16:37:39 -0700

    first commit

---
+++ app/(tabs)/bookmark.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs, Redirect } from 'expo-router'
+
+const Bookmark = () => {
+  return (
+    <View>
+      <Text>Bookmark</Text>
+    </View>
+  )
+}
+
+export default Bookmark
---
+++ components/EmptyState.jsx
@@ -0,0 +1,18 @@
+import { View, Text, Image } from 'react-native'
+import { images } from '../constants'
+import CustomButton from './CustomButton'
+import React from 'react'
+import { router } from 'expo-router'
+
+export default function EmptyState({title, subtitle}) {
+  return (
+    <View className="justify-center items-center px-4">
+        <Image source={images.empty} className="w-[270px] h-[215px]" resizeMode='contain' />
+        <Text className="text-xl font-psemibold text-white mt-2">{title}</Text>
+        <Text className="font-pmedium text-sm text-gray-100">{subtitle}</Text>
+
+        <CustomButton title="Create Video" handlePress={() => router.push('/create')} containerStyles="w-full my-5" />
+    </View>
+
+  )
+}
